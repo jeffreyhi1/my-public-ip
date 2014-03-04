2d18e1bc13f38b5a828e7f07bba6e0ce1840ee1c
Dan Rosen <danrosen25@gmail.com>
2014-03-04 13:17:10 -0700

    Updated options and logging

--- src/MyPublicIP.py
+++ src/MyPublicIP.py
@@ -13,6 +13,9 @@ import argparse
 import time
 import datetime
 import logging
+import signal
+
+version = "1.0"
 
 def mailIP(external_ip,timestamp):
     body = "IP: "+external_ip+"\nTime: "+datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
@@ -22,7 +25,7 @@ def mailIP(external_ip,timestamp):
     msg['To'] = recipient
     
     try:
-        mailServer = smtplib.SMTP(serverPort)
+        mailServer = smtplib.SMTP_SSL(serverPort)
         try:
             mailServer.login(username,password)
             try:
@@ -32,14 +35,50 @@ def mailIP(external_ip,timestamp):
                 logger.error("Could not send message to %s" % recipient)
         except (smtplib.SMTPHeloError, smtplib.SMTPAuthenticationError, smtplib.SMTPException):
             logger.error("Could not log in to: %s" % serverPort)
-        mailServer.quit()
+        try:
+            mailServer.quit()
+        except (smtplib.SMTPServerDisconnected):
+            logger.error("Server disconnect early.")
     except (smtplib.SMTPConnectError,socket.error):
         logger.error("Could not connect to server: %s" % serverPort)
+
+
+def exit_handler(signal,frame):
+    logger.info("Server Stopped")
+    sys.exit()
+
+def getAccount():
+    sys.stdout.write("Enter account: ")
+    username = sys.stdin.readline().rstrip()
+    password = getpass.getpass().rstrip()
+    sys.stdout.write("Recipient: ")
+    recipient = sys.stdin.readline().rstrip()
+    return username,password,recipient
+
+def saveAccount(filename):
+    print "Account saved"
     
+def loadAccount(filename):
+    print "Account loaded"
+    username="boo"
+    password="radley"
+    recipient="fake_email"
+    return username,password,recipient
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Send Public IP Address via Email',epilog='Author: Dan Rosen')
+    parser.add_argument('-m','--minutes',type=int,default=60)
+    parser.add_argument('-s','--server',default="smtp.aol.com")
+    parser.add_argument('-p','--port')
+    parser.add_argument('-l','--log',default="my_public_ip.log")
+    parser.add_argument('-f','--file',default=None)
+    parser.add_argument('-v','--save',action="store_true", help="requires filename")
+    parser.add_argument('--version',action='version',version='Version: %s' % version)
+    args = parser.parse_args()
+    
     logger = logging.getLogger('my_public_ip')
     logger.setLevel(logging.INFO)
-    logfh = logging.FileHandler('my_public_ip.log')
+    logfh = logging.FileHandler(args.log)
     logfh.setLevel(logging.INFO)
     logch = logging.StreamHandler()
     logch.setLevel(logging.ERROR)
@@ -49,28 +88,36 @@ if __name__ == '__main__':
     logger.addHandler(logfh)
     logger.addHandler(logch)
     
-    parser = argparse.ArgumentParser(description='Timing')
-    parser.add_argument('-m','--minutes',type=int,default=60)
-    parser.add_argument('-s','--server',default="smtp.aol.com")
-    parser.add_argument('-p','--port',type=int,default=587)
-    args = parser.parse_args()
-    sleep = args.minutes*60
-    serverPort = args.server+":"+str(args.port)
-    sys.stdout.write("Enter account: ")
-    username = sys.stdin.readline().rstrip()
-    password = getpass.getpass().rstrip()
-    sys.stdout.write("Recipient: ")
-    recipient = sys.stdin.readline().rstrip()
+    if(args.save is True):
+        if(args.file is None):
+            sys.exit("Saving file requires a filename.")
+        else:
+            username,password,recipient = getAccount()
+            saveAccount(args.file)
+    else:
+        if(args.file is None):
+            username,password,recipient = getAccount()
+        else:
+            username,password,recipient = loadAccount(args.file)
+    
+    sleeptime = args.minutes*60
+    if(args.port is None):
+        serverPort = args.server
+    else:
+        serverPort = args.server+":"+str(args.port)
     external_ip = ""
+    logger.info("Server Started")
+    signal.signal(signal.SIGINT, exit_handler)
+    print 'Press Ctrl+C to exit'
     while True:
         timestamp = time.time()
         logger.info('STUN request at %s' % datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S'))
         nat_type, new_external_ip, external_port = stun.get_ip_info()
         if(new_external_ip is None):
-            logger.error('STUN Failed')
+            logger.error('STUN request failed')
         elif(new_external_ip != external_ip):
-            logger.info('New IP Address %s' % new_external_ip)
+            logger.info('New IP address %s' % new_external_ip)
             mailIP(new_external_ip,timestamp)
         external_ip = new_external_ip
-        time.sleep(sleep)
+        time.sleep(sleeptime)
     
